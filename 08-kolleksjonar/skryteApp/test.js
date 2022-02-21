018d3b274e464f8817925ef449a06bfc7d43739d
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2022-02-21 12:32:54 +0100

    testing

---
+++ 08-kolleksjonar/skryteApp/test.js
@@ -0,0 +1,48 @@
+const skryteOrd = [
+    "sjenerøs",
+    "omgjengelig",
+    "ambisiøs",
+    "munter",
+    "hardtarbeidende",
+    "troverdig",
+    "tålmodig",
+    "optimistisk",
+    "følsom",
+    "sosial",
+    "besluttsom",
+    "morsom",
+    "blid",
+    "vakker",
+    "lekende",
+    "elskverdig",
+    "talentfull",
+    "begavet"
+];
+
+let person = prompt("Hvem vil du skryte av?");
+let antallSkryteord = parseInt(prompt("Hvor mange skryteord?"));
+
+function skrytAv(person, antallSkryteord) {
+    let skryteSetning = "Kjære, <span class='navn'>" + person + "</span>!<br> Du er ";
+    for (let i = 0; i < antallSkryteord; i++) {
+        let plassering = tilfeldigTall(0,skryteOrd.length-1);
+        let utvaltOrd = skryteOrd[plassering];
+        skryteOrd.splice(plassering,1);
+        if(i<antallSkryteord-3) {
+            skryteSetning += ", ";
+        }
+        else {
+            skryteSetning = " og " + skryteSetning;
+        }
+    }
+    console.log(skryteSetning);
+    document.getElementById("skrytUt").innerHTML = skryteSetning;
+}
+
+skrytAv(person, antallSkryteord);
+
+function tilfeldigTall(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
