35d90863b75337b958a70cbba8845eb584e84228
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2022-03-23 19:58:00 +0100

    JSON filmbutikk (og bok)

---
+++ 08-kolleksjonar/JSON/json-filmbutikk-data.js
@@ -0,0 +1,10 @@
+var filmbutikken = {
+    "navn" : "Filmbutikken",
+    "versjon" : "0.1", 
+    "filmer" : 
+        [
+            {"id" : "1","tittel" : "Ben Hur","poster" : "bileter/ben_hur.jpg","pris" : 129}, 
+            {"id" : "2","tittel" : "Star Wars","poster": "bileter/star_wars.jpg","pris" : 149}, 
+            {"id" : "3","tittel" : "King Kong","poster": "bileter/king_kong.jpg","pris" : 99}
+        ] 
+};
