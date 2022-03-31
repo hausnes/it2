c119429e03b79b12170663d85560e5a1d455e125
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2022-03-31 10:35:03 +0200

    Lagt til å hente ut bilete

--- 08-kolleksjonar/API/StudioGhibli/kode.js
+++ 08-kolleksjonar/API/StudioGhibli/kode.js
@@ -24,6 +24,9 @@ request.onload = function () {
 			const h1 = document.createElement('h1');
 			h1.textContent = movie.title;
 
+			const bilete = document.createElement('img');
+			bilete.src = movie.image;
+
 			const p = document.createElement('p');
 			movie.description = movie.description.substring(0, 300);
 			p.textContent = `${movie.description}...`;
@@ -33,6 +36,7 @@ request.onload = function () {
 
 			container.appendChild(card);
 			card.appendChild(h1);
+			card.appendChild(bilete);
 			card.appendChild(p);
 			card.appendChild(p2);
   		})
