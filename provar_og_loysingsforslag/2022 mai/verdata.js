f5e0d56b7eda8acca49d7aaf46f1c63ec87c205e
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2022-05-19 18:14:18 +0200

    Create verdata.js

---
+++ provar_og_loysingsforslag/2022 mai/verdata.js
@@ -0,0 +1,40 @@
+var ver = {
+    "navn" : "Verstasjon",
+    "versjon" : "1.0", 
+    "verdataBergen" : 
+        [
+            {"id" : "Bergen","veke" : 1,"dag": 1,"nedbørsmengde" :  89, "temperaturHigh" :  8, "temperaturLow" :  4}, 
+            {"id" : "Bergen","veke" : 1,"dag": 2,"nedbørsmengde" : 129, "temperaturHigh" :  7, "temperaturLow" :  3}, 
+            {"id" : "Bergen","veke" : 1,"dag": 3,"nedbørsmengde" :  33, "temperaturHigh" :  6, "temperaturLow" :  4},
+            {"id" : "Bergen","veke" : 1,"dag": 4,"nedbørsmengde" :   2, "temperaturHigh" :  2, "temperaturLow" :  1}, 
+            {"id" : "Bergen","veke" : 1,"dag": 5,"nedbørsmengde" :   0, "temperaturHigh" :  1, "temperaturLow" :  0}, 
+            {"id" : "Bergen","veke" : 1,"dag": 6,"nedbørsmengde" :   0, "temperaturHigh" :  0, "temperaturLow" : -1},
+            {"id" : "Bergen","veke" : 1,"dag": 7,"nedbørsmengde" :   0, "temperaturHigh" : -2, "temperaturLow" : -3}, 
+            {"id" : "Bergen","veke" : 2,"dag": 1,"nedbørsmengde" :   0, "temperaturHigh" : -5, "temperaturLow" : -9}, 
+            {"id" : "Bergen","veke" : 2,"dag": 2,"nedbørsmengde" :   0, "temperaturHigh" : -2, "temperaturLow" : -3},
+            {"id" : "Bergen","veke" : 2,"dag": 3,"nedbørsmengde" :  29, "temperaturHigh" :  5, "temperaturLow" :  3}, 
+            {"id" : "Bergen","veke" : 2,"dag": 4,"nedbørsmengde" : 109, "temperaturHigh" :  7, "temperaturLow" :  4}, 
+            {"id" : "Bergen","veke" : 2,"dag": 5,"nedbørsmengde" :  32, "temperaturHigh" : 10, "temperaturLow" :  8},
+            {"id" : "Bergen","veke" : 2,"dag": 6,"nedbørsmengde" :  44, "temperaturHigh" :  7, "temperaturLow" :  5}, 
+            {"id" : "Bergen","veke" : 2,"dag": 7,"nedbørsmengde" : 149, "temperaturHigh" :  8, "temperaturLow" :  2}
+            // Mønsteret fortsetter videre her ...
+        ],
+        "verdataVoss" : 
+        [
+            {"id" : "Voss","veke" : 1,"dag": 1,"nedbørsmengde" :   0, "temperaturHigh" :  0, "temperaturLow" : -2}, 
+            {"id" : "Voss","veke" : 1,"dag": 2,"nedbørsmengde" :   0, "temperaturHigh" : -3, "temperaturLow" : -5}, 
+            {"id" : "Voss","veke" : 1,"dag": 3,"nedbørsmengde" :   0, "temperaturHigh" : -7, "temperaturLow" : -9},
+            {"id" : "Voss","veke" : 1,"dag": 4,"nedbørsmengde" :   2, "temperaturHigh" :  1, "temperaturLow" : -2}, 
+            {"id" : "Voss","veke" : 1,"dag": 5,"nedbørsmengde" :   3, "temperaturHigh" :  2, "temperaturLow" :  1}, 
+            {"id" : "Voss","veke" : 1,"dag": 6,"nedbørsmengde" :   5, "temperaturHigh" :  2, "temperaturLow" :  0},
+            {"id" : "Voss","veke" : 1,"dag": 7,"nedbørsmengde" :  10, "temperaturHigh" :  4, "temperaturLow" :  2}, 
+            {"id" : "Voss","veke" : 2,"dag": 1,"nedbørsmengde" :  20, "temperaturHigh" :  4, "temperaturLow" :  0}, 
+            {"id" : "Voss","veke" : 2,"dag": 2,"nedbørsmengde" :  25, "temperaturHigh" :  5, "temperaturLow" :  2},
+            {"id" : "Voss","veke" : 2,"dag": 3,"nedbørsmengde" :  66, "temperaturHigh" :  7, "temperaturLow" :  3}, 
+            {"id" : "Voss","veke" : 2,"dag": 4,"nedbørsmengde" :  34, "temperaturHigh" :  5, "temperaturLow" :  2}, 
+            {"id" : "Voss","veke" : 2,"dag": 5,"nedbørsmengde" :  33, "temperaturHigh" :  4, "temperaturLow" :  1},
+            {"id" : "Voss","veke" : 2,"dag": 6,"nedbørsmengde" :  89, "temperaturHigh" :  8, "temperaturLow" :  5}, 
+            {"id" : "Voss","veke" : 2,"dag": 7,"nedbørsmengde" : 209, "temperaturHigh" : 12, "temperaturLow" :  9}
+            // Mønsteret fortsetter videre her ...
+        ] 
+};
